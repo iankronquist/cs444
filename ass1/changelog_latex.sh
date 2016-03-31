3bbfb59c0f65f3496a75ba8dfa45e923ca39fb88
Ian Kronquist <iankronquist@gmail.com>
2016-03-30 23:40:24 -0700

    Add script which generates changelog latex doc

---
+++ ass1/changelog_latex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+OUTPUT=git_log.tex
+# Find remote URL for hashes (designed for GitHub-hosted projects)
+origin=`git config remote.origin.url`
+base=`dirname "$origin"`/`basename "$origin" .git`
+
+# Output LaTeX table in chronological order
+echo '\\begin{tabular}{l l l}\\textbf{Detail} & \\textbf{Author} & \\textbf{Description}\\\\ \hline' > $OUTPUT
+git log --pretty=format:"\\href{$base/commit/%H}{%h} & %an & %s\\\\\\hline" --reverse | sed 's/_/\\_/g' >> $OUTPUT
+echo "\end{tabular}" >> $OUTPUT
