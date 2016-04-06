27956559806288b2f0404df46c995b9b71c2b012
Ian Kronquist <iankronquist@gmail.com>
2016-04-05 17:35:21 -0700

    Add arguments to qemu to ssh into the vm

--- NOTES.md
+++ NOTES.md
@@ -7,3 +7,7 @@ CONFIG_CROSS_COMPILE="-kronquii-hw1"
 
 qemu-img create  core-image-lsb-sdk-qemux86.ext3 512M
 mkfs.ext3 core-image-lsb-sdk-qemux86.ext3
+
+Copying files to the vm:
+Redirect host 7222 -> qemu 22.
+$ scp -P 7222 -r ass3 root@localhost:/home/root
--- qemu.sh
+++ qemu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qemu-system-i386 -gdb tcp::5572 -S -nographic -kernel linux-yocto-3.14/arch/x86/boot/bzImage -drive file=core-image-lsb-sdk-qemux86.ext3,if=virtio -enable-kvm -net none -usb -localtime --no-reboot --append "root=/dev/vda rw console=ttyS0 debug"
+qemu-system-i386 -gdb tcp::5572 -S -nographic -kernel linux-yocto-3.14/arch/x86/boot/bzImage -drive file=core-image-lsb-sdk-qemux86.ext3,if=virtio -enable-kvm -usb -localtime --no-reboot --append "root=/dev/vda rw console=ttyS0 debug" -redir tcp:7222::22
