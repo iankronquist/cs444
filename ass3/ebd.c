b4b2feda617fb3a41dc3428c9bbde3fe403577ab
Ian Kronquist <iankronquist@gmail.com>
2016-04-05 20:19:12 -0700

    Working code for assignment 3

--- ass3/ebd.c
+++ ass3/ebd.c
@@ -20,7 +20,7 @@ static int major_num = 0;
 module_param(major_num, int, 0);
 static int logical_block_size = 512;
 module_param(logical_block_size, int, 0);
-static int num_sectors;
+static int num_sectors = 1024;
 module_param(num_sectors, int, 0);
 
 // Create global queue for servicing requests.
@@ -31,34 +31,50 @@ static struct request_queue *Queue;
 // Key size in bytes
 #define KEY_SIZE 32
 
-/* A global structure representing the device.
-*/
+// A global structure representing the ebd0 device.
 static struct ebd_device {
-    unsigned long size;
-    spinlock_t lock;
-    u8 *data;
-    u8 key[KEY_SIZE];
-    struct crypto_blkcipher *blkcipher;
-    struct scatterlist sg[2];
-    struct gendisk *gd;
+    unsigned long size;                 // Size in bytes of the device.
+    spinlock_t lock;                    // A spinlock to control access.
+    u8 *data;                           // The device data.
+    // I may remove this.
+    u8 key[KEY_SIZE];                   // The AES key for encrypting the data.
+    struct crypto_cipher *blkcipher; // Kernel CryptoAPI AES block cipher.
+    struct scatterlist sg[2];           // Data structure used for encryption.
+    struct gendisk *gd;                 // Generic disk structure.
 } Device;
 
-// Handle an I/O request
+static void debug_print_bytes(char *a, unsigned long len) {
+    unsigned long i;
+    printk("edb0:");
+    for (i = 0; i < len; ++i) {
+        printk("%c", a[i]);
+    }
+    printk("\n");
+}
+
+// Handle I/O requests
 static void ebd_transfer(struct ebd_device *dev, sector_t sector,
         unsigned long sector_num, char *buffer, int write) {
     unsigned long offset = sector * logical_block_size;
     unsigned long num_bytes = sector_num * logical_block_size;
+    unsigned long block_size = crypto_cipher_blocksize(dev->blkcipher);
+    unsigned long i;
     if ((offset + num_bytes) > dev->size) {
-        printk(KERN_NOTICE,
-            "ebd: Writing beyond the end of the device. Offset: %ld. Number of bytes: %ld\n",
+        printk("ebd: Writing beyond the end of the device. Offset: %ld. "
+               "Number of bytes: %ld\n",
             offset, num_bytes);
     }
     if (write) {
-        int len;
-        char result[64];
-        memcpy(dev->data + offset, buffer, num_bytes);
+        char *destination = dev->data + offset;
+        for (i = 0; i < num_bytes; i += block_size) {
+            crypto_cipher_encrypt_one(dev->blkcipher, &destination[i],
+                &buffer[i]);
+        }
     } else {
-        memcpy(buffer, dev->data + offset, num_bytes);
+        char *source = dev->data + offset;
+        for (i = 0; i < num_bytes; i += block_size) {
+            crypto_cipher_decrypt_one(dev->blkcipher, &buffer[i], &source[i]);
+        }
     }
 }
 
@@ -67,7 +83,7 @@ static void ebd_request(struct request_queue *queue) {
     req = blk_fetch_request(queue);
     while (req != NULL) {
         if (req->cmd_type != REQ_TYPE_FS) {
-            printk(KERN_NOTICE, "ebd: Skipping non-CMD request\n");
+            printk("ebd: Skipping non-CMD request\n");
             __blk_end_request_all(req, EIO);
             continue;
         }
@@ -96,56 +112,52 @@ static struct block_device_operations ebd_ops = {
 
 static int __init ebd_init(void) {
     unsigned int err;
+    // FIXME: Generate random key.
+    u8 *test_key = "0123456789abcdef0123456789abcdef";
+
     // Initialize the device spin lock. Only one process should be able to
     // read/write to the device at time.
     spin_lock_init(&Device.lock);
 
+    Device.size = num_sectors * logical_block_size;
     // Allocate memory to hold encrypted contents.
     if (!(Device.data = vmalloc(Device.size))) {
-        printk(KERN_NOTICE, "Failed to allocate memory");
+        printk("ebd: Failed to allocate memory\n");
         goto free_mem;
     }
 
     // Allocate block cipher machinery.
-    Device.blkcipher = crypto_alloc_blkcipher("aes", 0, CRYPTO_ALG_ASYNC);
+    Device.blkcipher = crypto_alloc_cipher("aes", 0, CRYPTO_ALG_ASYNC);
     if (IS_ERR(Device.blkcipher)) {
-        printk(KERN_NOTICE, "Failed to load aes cryptographic transform");
+        printk("ebd: Failed to load aes cryptographic transform\n");
         goto free_mem;
     }
 
     // Initialize the scatter/gather table used to perform the encryption.
     //sg_init_table(Device.sg, );
 
-    // FIXME: Generate random key.
-    // Generate a key.
-    u8 *test_key = "0123456789abcdef0123456789abcdef";
-    memcpy(Device.key, test_key, KEY_SIZE);
-
     // Set the key
-    err = crypto_blkcipher_setkey(Device.blkcipher, Device.key,
-        KEY_SIZE);
-    if (err) {
-        printk(KERN_NOTICE, "Failed to set key");
+    err = crypto_cipher_setkey(Device.blkcipher, test_key, KEY_SIZE);
+    if (err != 0) {
+        printk("Failed to set key");
         goto free_mem;
     }
 
     // Create the queue of memory blocks.
     Queue = blk_init_queue(ebd_request, &Device.lock);
     if (Queue == NULL) {
-        printk(KERN_NOTICE, "Failed to set key");
+        printk("ebd: Failed to initialize request queue\n");
         goto free_mem;
     }
 
     // Set the block size.
     blk_queue_logical_block_size(Queue, logical_block_size);
 
-    // Add the queue to the device structure.
-    Device.gd->queue = Queue;
 
     // Register the driver major number, a unique numeric id.
     major_num = register_blkdev(major_num, "ebd");
     if (major_num < 0) {
-        printk(KERN_WARNING, "ebd: could not get major number for device");
+        printk("ebd: could not get major number for device\n");
         goto free_mem;
     }
 
@@ -154,7 +166,10 @@ static int __init ebd_init(void) {
     if (Device.gd == 0) {
         goto remove_dev;
     }
-    
+
+    // Add the queue to the device structure.
+    Device.gd->queue = Queue;
+
     // Set up the disk properly.
     Device.gd->major = major_num;
     Device.gd->first_minor = 0;
@@ -166,23 +181,27 @@ static int __init ebd_init(void) {
 
     add_disk(Device.gd);
 
-    //
+    // Exit successfully.
     return 0;
 
 remove_dev:
+    // Unregister the device.
     unregister_blkdev(major_num, "ebd");
 free_mem:
+    // Free storage.
     vfree(Device.data);
-    return ENOMEM;
+    // Apparently returning error numbers is kernel convention.
+    return -ENOMEM;
 }
 
 static void __exit ebd_exit(void) {
+    // Remove the disk structure
     del_gendisk(Device.gd);
+    // Update disk reference count.
     put_disk(Device.gd);
     unregister_blkdev(major_num, "ebd");
     blk_cleanup_queue(Queue);
     vfree(Device.data);
-
 }
 
 module_init(ebd_init);
